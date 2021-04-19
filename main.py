3826088dacd730dae69ee25ecdffbcbd284c0c57
Hien Pham <phaminhien@hotmail.com>
2021-04-19 15:39:06 +0700

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+PINK = "#e2979c"
+RED = "e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_DURATION = 25
+SHORT_BREAK_DURATION = 5
+LONG_BREAK_DURATION = 20
+
+window = Tk()
+window.title("Pomodoro")
+
+window.mainloop()
